6b46805505d3febf93ac32fec32eb66c49306b26
Sziroky <p.sziroky.us@gmail.com>
2024-03-18 12:55:32 +0100

    script to extract zip files with unique id into specified directory v0.2

--- extract_satellites_archive.py
+++ extract_satellites_archive.py
@@ -1,10 +1,9 @@
-import os
-import zipfile
-import uuid
-
 
 extract_folder = 'D:\Praca_magisterska\scripts\images'
 zip_file = 'D:\Praca_magisterska\scripts\satellite_imagery_archives'
+import os
+import zipfile
+import uuid
 
 def extract_archives(zip_files_dir: str, extract_folder: str):
     for root, dirs, files in os.walk(zip_files_dir):
@@ -12,17 +11,21 @@ def extract_archives(zip_files_dir: str, extract_folder: str):
             print(f"started extracting files from {file}\n{'-'*100}")
             if file.endswith('.zip'):
                 with zipfile.ZipFile(os.path.join(root, file), mode='r') as archive:
-                    archive.extractall(extract_folder)
-                    uniqueid = str(uuid.uuid4())[:4]
-                    for archive_file in os.listdir(extract_folder):
-                        archive.extractall(extract_folder)
-                        filename, file_extension = os.path.splitext(archive_file)
-                        print(filename)
-                        extracted_file_path = os.path.join(extract_folder, archive_file)
-                        new_filename = f"{filename}_{uniqueid}{file_extension}"
-                        print(uniqueid)
-                        os.rename(extracted_file_path, os.path.join(extract_folder, new_filename))
-
-
-
-    print(f'Successfully extracted into {extract_folder}')
+                    unique_id = str(uuid.uuid4())[:4]
+                    extract_path = os.path.join(extract_folder, f"{file[:-4]}_{unique_id}")
+                    archive.extractall(extract_path)
+        for root, dirs, files in os.walk(extract_folder):
+            for dir in dirs:
+                id = dir[-4:]
+                print(f"{'-' * 50}{id}{'-' * 100}")
+                path_to_dir = os.path.join(root, dir)
+                for file in os.listdir(os.path.join(root, dir)):
+                    filename, file_extension = os.path.splitext(file)
+                    new_filename = f"{filename}_{id}{file_extension}"
+                    src = os.path.join(path_to_dir, file)
+                    dst = os.path.join(extract_folder, new_filename)
+                    # print(f'{src}\n--> {dst}')
+                    os.rename(src, dst)
+        for root,dirs,files in os.walk(extract_folder):
+            for dir in dirs:
+                os.rmdir(os.path.join(root,dir))
